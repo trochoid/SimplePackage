7d396a2185694bbbf9ebf6575b5adf3d02178218
trochoid <trochoid@users.noreply.github.com>
2022-03-31 05:08:07 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SimplePackage",
     platforms: [ .iOS(.v13) ],
-    //products: [ .library(name: "SimplePackage", targets: ["SimplePackage"]) ],
+    products: [ .library(name: "SimplePackage", targets: ["SimplePackage"]) ],
     //dependencies: [],
     targets: [ .target(name: "SimplePackage", path: "") ]
 )

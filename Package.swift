4b3bfab025769cf5e0a2330f8f99b6d2d456539e
trochoid <trochoid@users.noreply.github.com>
2022-03-31 04:21:04 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "SimplePackage",
+    platforms: [ .iOS(.v13) ],
+    products: [ .library(name: "SimplePackage", targets: ["SimplePackage"]) ],
+    dependencies: [],
+    targets: [ .target(name: "SimplePackage", dependencies: []) ]
+)

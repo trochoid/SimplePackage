15426743a1bae4db44d3c418d226955308bda39c
trochoid <trochoid@users.noreply.github.com>
2022-03-31 21:06:04 -0700

    Update SimpleFile.swift

--- SimpleFile.swift
+++ SimpleFile.swift
@@ -1,5 +1,31 @@
-import SwiftUI
 
-public func bananoids() {
-    print("floopy")
+public class FooA {
+    public var a = "a"
+    public private(set) var b = "b"
+    internal var c = "c"
+    static public var d = "d"
+    static public private(set) var e = "e"
+    public init(info: String) {
+        self.b = info
+    }
+}
+
+public class FooB {
+    public var a = "a"
+}
+public func makeFooB() -> FooB { FooB() }
+
+open class FooC {
+    public var a = "a"
+    public init() {}
+}
+
+public struct FooD {
+    public var a = "a"
+}
+
+public class FooE {
+    public var a: String
+    init() { a = "internal" }
+    public init(info: String) { a = info }
 }
